7de33e4f9695869cb70ec424a3bd995a9973673e
Christie <christiecol7@gmail.com>
2020-11-19 18:36:33 -0500

    Re-installed dependencies, add homepage folder and edit app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HomePage } from "./home-page/HomePage";
 
 function App() {
-  const [bacon, setBacon] = useState(null);
-
-  useEffect(() => {
-    fetch('/bacon')
-      .then(res => res.json())
-      .then(data => setBacon(data));
-  }, []);
-
-  return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
+  return (
+    <>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
+  );
 }
+
+// const [bacon, setBacon] = useState(null);
+
+// useEffect(() => {
+//   fetch('/bacon')
+//     .then(res => res.json())
+//     .then(data => setBacon(data));
+// }, []);
+
+// return <div>{bacon ? bacon : `...where's my stuff?...`}</div>;
 
 export default App;
