7a58123f399bc073e8dd609895a967c1165d80c0
Christie <christiecol7@gmail.com>
2020-11-20 10:51:38 -0500

    added categories component

---
+++ client/src/categories/Categories.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// displays our categories
+export const Categories = () => {
+  return (
+    <div>
+      <h3>Men</h3>
+      <h3>Women</h3>
+      <h3>Unisex</h3>
+      <h3>Accessories</h3>
+    </div>
+  );
+};
