0c6f42ea39b68ca52d3f528abe0d9c50ab9966e3
Julien Bidar <julien.bidar@gmail.com>
2020-11-21 16:01:53 -0500

    allItems displaying not styled

--- client/src/components/all-items/AllItemsPage.jsx
+++ client/src/components/all-items/AllItemsPage.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import Items from "./Items";
 
 const AllItemsPage = () => {
-  return <div>All items ...</div>;
+  const [items, setItems] = useState([]);
+  const fetchItems = async () => {
+    try {
+      let data = await fetch("/items");
+      data = await data.json();
+      let items = data.data;
+      setItems(items);
+      console.log(items);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  if (!items) {
+    return <p>loading...</p>;
+  }
+
+  return items.map((item) => {
+    return <Items item={item} />;
+  });
 };
 export default AllItemsPage;
--- client/src/components/all-items/items.jsx
+++ client/src/components/all-items/items.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import styled from "styled-components";
 import { COLORS } from "../../constants";
 
-const Items = () => {
-  return null;
+const Items = ({ item }) => {
+  const { name, price, imageSrc, category, numInStock, _id } = item;
+
+  // we need to acces all brands to get the name of the brand matching with companyId
+  const brand = item.companyId;
+
+  if (!item) {
+    return null;
+  }
+
+  return (
+    <>
+      <div>
+        <h1>{name}</h1>
+        <p>{category}</p>
+      </div>
+      <div>
+        <img src={imageSrc} alt={name} />
+      </div>
+      <div>
+        <p>{price}</p>
+      </div>
+    </>
+  );
 };
 
 export default Items;
