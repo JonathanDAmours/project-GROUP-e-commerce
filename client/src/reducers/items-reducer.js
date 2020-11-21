0ace16859037fe169aae2b81d2c9ec119845ab5e
MitchellWestcott <westcott.mitchell@gmail.com>
2020-11-21 16:23:44 -0500

    Updated reducers

--- client/src/reducers/items-reducer.js
+++ client/src/reducers/items-reducer.js
@@ -2,8 +2,34 @@ const initialState = {};
 
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
+    case "ADD_ITEM": {
+      const currentItem = state[action.item._id];
+      return {
+        ...state,
+        [action.item._id]: {
+          ...action.item,
+          quantity: currentItem ? currentItem.quantity + 1 : 1,
+        },
+      };
+    }
+    case "REMOVE_ITEM": {
+      const stateCopy = { ...state };
+      delete stateCopy[action.item._id];
+      return stateCopy;
+    }
+    case "UPDATE_QUANTITY": {
+      return {
+        ...state,
+        [action.item._id]: {
+          ...state[action.item._id],
+          quantity: action.item.quantity,
+        },
+      };
+    }
     default: {
       return state;
     }
   }
 }
+
+export const getStoreItemArray = (state) => Object.values(state);
