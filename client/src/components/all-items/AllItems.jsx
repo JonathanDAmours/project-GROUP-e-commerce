c1dc9d6f5db7e84ed7f9383b22f01fdc469b368b
Christie <christiecol7@gmail.com>
2020-11-21 11:45:52 -0500

    cleaning homepage

--- client/src/components/all-items/AllItems.jsx
+++ client/src/components/all-items/AllItems.jsx
@@ -10,17 +10,9 @@ export const AllItems = () => {
   return (
     <div>
       <AllItemsDiv>
-        <CyclistDiv>
-          <CyclistButton>
-            <ImgDiv>
-              <CyclistImg src={Cyclist} />
-            </ImgDiv>
-            <CyclistText>Shop All Items</CyclistText>
-          </CyclistButton>
-        </CyclistDiv>
         <ViewAllDiv>
           <ViewAllA aria-label="view all store items" href="#">
-            View all <FiArrowRight />
+            Shop All Items <FiArrowRight />
           </ViewAllA>
         </ViewAllDiv>
       </AllItemsDiv>
@@ -34,55 +26,8 @@ const AllItemsDiv = styled.div`
   align-items: center;
 
   width: 100%;
-  margin-top: 3rem;
+  margin-top: 7rem;
   margin-bottom: 7rem;
-`;
-
-const CyclistButton = styled.button`
-  all: unset;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  width: 100%;
-`;
-
-const CyclistDiv = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: all 0.2s ease-in-out;
-  margin-right: 1.5rem;
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-
-const ImgDiv = styled.div`
-  margin-bottom: 10px;
-  background: rgba(0, 0, 0, 0.3);
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-`;
-
-const CyclistImg = styled.img`
-  position: relative;
-  z-index: -10;
-  border-radius: 10px;
-  max-height: 18rem;
-`;
-
-const CyclistText = styled.h3`
-  position: absolute;
-  z-index: 10;
-  color: white;
-  font-size: 2.5rem;
-
-  &:hover {
-    color: ${COLORS.orange};
-  }
 `;
 
 const ViewAllDiv = styled.div``;
@@ -92,7 +37,8 @@ const ViewAllA = styled.a`
   display: flex;
   align-items: center;
   cursor: pointer;
-  font-size: 1.5rem;
+  font-size: 3rem;
+  font-weight: bold;
   color: ${COLORS.orange};
   margin-left: 0;
   &:hover {
