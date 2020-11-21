0d42480682aa9cdab307d3e83260ab4728db61ac
Christie <christiecol7@gmail.com>
2020-11-21 17:05:51 -0500

    rendered brands names

--- client/src/actions.js
+++ client/src/actions.js
@@ -2,12 +2,3 @@ export const addToCart = (item) => ({
   type: "ADD_ITEM",
   item,
 });
-
-// Brands:
-//get all brands data
-export const receiveAllBrands = (brands) => {
-  return {
-    type: "RECEIVE_ALL_BRANDS",
-    brands,
-  };
-};
---
+++ client/src/components/brands/IndividualBrand.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+export const IndividualBrand = ({ brand }) => {
+  const { name } = brand;
+  return <div>{name}</div>;
+};
