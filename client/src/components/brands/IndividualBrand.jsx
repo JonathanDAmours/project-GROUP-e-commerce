bc56d5938cfb879330649280979d03c72214b367
Christie <christiecol7@gmail.com>
2020-11-22 11:52:42 -0500

    worked on individualbrand

--- client/src/components/brands/IndividualBrand.jsx
+++ client/src/components/brands/IndividualBrand.jsx
@@ -1,6 +1,60 @@
 import React from "react";
+import styled from "styled-components";
+
+import { COLORS, BOX_SHADOW } from "../../constants";
 
 export const IndividualBrand = ({ brand }) => {
   const { name } = brand;
-  return <div>{name}</div>;
+  return (
+    <>
+      <Button>
+        <BrandDiv>
+          <BrandName>{name}</BrandName>
+        </BrandDiv>
+      </Button>
+    </>
+  );
 };
+
+const Brands = styled.div`
+  display: flex;
+  // flex-direction: row;
+  // justify-content: center;
+  // flex-wrap: wrap;
+`;
+
+const BrandDiv = styled.div`
+  display: flex;
+  justify-content: center;
+
+  line-height: 1.4rem;
+  text-align: center;
+  font-size: 1.5rem;
+  color: ${COLORS.orange};
+`;
+
+const Button = styled.button`
+  all: unset;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  // background: rgb(255,229,210);
+  // background: linear-gradient(180deg, rgba(255,229,210,0.3) 3%, rgba(212,94,9,0.3) 94%);
+  margin: 1.5rem;
+  width: 300px;
+  border-radius: 10px;
+  padding: 2rem;
+
+  box-shadow: ${BOX_SHADOW.regularButton};
+
+  &:hover {
+    box-shadow: ${BOX_SHADOW.hoverButton};
+  }
+`;
+
+const BrandName = styled.h2``;
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,8 @@
 export const COLORS = {
   orange: "#D45E09",
 };
+
+export const BOX_SHADOW = {
+  regularButton: "0px 0px 12px 1px rgba(0, 0, 0, 0.33)",
+  hoverButton: "0px 0px 20px 1px rgba(0, 0, 0, 0.5)",
+};
