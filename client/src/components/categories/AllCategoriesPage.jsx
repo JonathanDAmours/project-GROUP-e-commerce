8431d938ddb5352b108f52dba4ea49ae946203d7
Jonathan D'Amours <Jonathan@MacBook-Pro-Jonathan-DAmours.local>
2020-11-22 00:03:04 -0500

    Minor fixes

--- client/src/components/categories/AllCategoriesPage.jsx
+++ client/src/components/categories/AllCategoriesPage.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import styled from "styled-components";
 
 const AllCategoriesPage = () => {
-  return <div>All categories ...</div>;
+  return <Wrapper>All categories ...</Wrapper>;
 };
+
+const Wrapper = styled.div`
+padding: 20px 40px;
+`;
 export default AllCategoriesPage;
