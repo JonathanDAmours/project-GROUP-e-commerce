4980bbc2e8cfd49ba0f3449e0b9458ed90aca439
Jonathan D'Amours <Jonathan@MacBook-Pro-Jonathan-DAmours.local>
2020-11-23 11:53:57 -0500

    Items by cat

--- client/src/components/categories/ItemsByCategory.jsx
+++ client/src/components/categories/ItemsByCategory.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
+import styled from "styled-components";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import Items from "../all-items/Items";
+import Items from "../all-items/items";
 
 export const ItemsByCategory = () => {
   const { categoryName } = useParams();
   const [itemsByCategory, setItemsByCategory] = useState([]);
   console.log(itemsByCategory);
+  const [items, setItems] = useState([]);
 
+  
   useEffect(() => {
-    const fetchCategoryByCategoryName = async () => {
+  const fetchCategoryByCategoryName = async () => {
       try {
         let data = await fetch(`/categories/${categoryName}`);
         data = await data.json();
@@ -23,12 +26,78 @@ export const ItemsByCategory = () => {
     fetchCategoryByCategoryName();
   }, [categoryName]);
 
+
+
+  if (!items) {
+    return <p>loading...</p>;
+  }
+
   return (
-    <div>
+    <Wrapper>
+      <NextPrevious>
+      </NextPrevious>
+      <ItemsWrap>
       {itemsByCategory.map((item) => {
         return <Items key={item._id} item={item} />;
       })}
       ;
-    </div>
+      </ItemsWrap>
+    </Wrapper>
   );
 };
+
+const PrevBtn = styled.button`
+  padding: 5px 10px;
+  border: none;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+  &:disabled {
+    cursor: not-allowed;
+    background-color: grey;
+  }
+`;
+
+const NextBtn = styled.button`
+  padding: 5px 10px;
+  border: none;
+  background-color: black;
+  color: white;
+  cursor: pointer;
+`;
+
+const NextPrevious = styled.div`
+  margin: 20px 0;
+`;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    margin: 0 auto;
+  }
+
+  @media screen and (max-width: 900px) {
+    margin: 0 auto;
+  }
+
+  @media screen and (max-width: 735px) {
+    margin: 0 auto;
+  }
+
+  @media screen and (max-width: 600px) {
+    margin: 0 auto;
+  }
+`;
+
+const ItemsWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+
+  width: 90vw;
+`;
